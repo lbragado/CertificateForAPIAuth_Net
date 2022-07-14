33bb0221a4228bf9a87201938704887f4f695373
lbragado <luis.bragado.mx@gmail.com>
2022-07-14 17:29:29 -0500

    Cargando proyecto a github

---
+++ OauthApp/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace OauthApp.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        //This resource is For all types of role
+        [Authorize(Roles = "SuperAdmin, Admin, User")]
+        [HttpGet]
+        [Route("api/values/getvalues")]
+        public IHttpActionResult GetValues()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var roles = identity.Claims
+                        .Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value);
+            var LogTime = identity.Claims
+                      .FirstOrDefault(c => c.Type == "LoggedOn").Value;
+            return Ok("Hello: " + identity.Name + ", " +
+                "Your Role(s) are: " + string.Join(",", roles.ToList()) +
+                "Your Login time is :" + LogTime);
+        }
+    }
+}
